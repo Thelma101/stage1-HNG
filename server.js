87cd14815ae04bb8d3577375be99a7e1edaa1064
DCTP-BE <akpata.thelma@gmail.com>
2024-07-01 20:56:49 +0100

    stage1

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+app.get('/api/hello?',  (req, res) => {
 
+})
 
+const url = 'https://api.ipstack.com/check?access_key={PASTE_YOUR_API_KEY_HERE}';
+const options = {
+	method: 'GET'
+};
 
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
 
 app.listen(port, () => {
     console.log(`Server is connected on ${port}`);
