eb2f0d1b1db29d75d40c0343bc29996abb612e70
DCTP-BE <akpata.thelma@gmail.com>
2024-07-02 16:09:58 +0100

    ipstack fix

--- index.js
+++ index.js
@@ -4,49 +4,36 @@ const app = express();
 const port = 3001;
 
 const ipstackAccessKey = '7d065c730b8326a1b41caa942cf628fa';
+const ipstackApiUrl = `http://api.ipstack.com/check?access_key=${ipstackAccessKey}`;
 const meteoWeatherAPI = 'https://api.open-meteo.com/v1/forecast?';
 
 app.get('/api/hello', async (req, res) => {
-    const personName = req.query.personName;
+    const visitorName = req.query.visitor_name;
 
-    if (!personName) {
+    if (!visitorName) {
         return res.status(400).json({
             message: 'Please enter your name in the URL'
         });
     }
 
     try {
-        // Get client's IP address from request headers
-        const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-
-        if (!clientIp) {
-            return res.status(400).json({
-                message: 'Could not determine client IP address'
-            });
-        }
-
         // Fetch location data using Axios
-        const ipstackApiUrl = `http://api.ipstack.com/${clientIp}?access_key=${ipstackAccessKey}`;
         const locationResponse = await axios.get(ipstackApiUrl);
-        const { city, latitude, longitude } = locationResponse.data;
-
-        if (!latitude || !longitude) {
-            throw new Error('Location data not available');
-        }
+        const { ip, city, latitude, longitude } = locationResponse.data;
 
         // Fetch temperature data using latitude and longitude
         const weatherResponse = await axios.get(`${meteoWeatherAPI}&latitude=${latitude}&longitude=${longitude}&current=temperature_2m&hourly=temperature_2m&forecast_days=1`);
         const currentTemperature = weatherResponse.data.current.temperature_2m;
 
-        const greeting = `Hello ${personName}!, the temperature is ${currentTemperature}°C in ${city}`;
+        const greeting = `Hello ${visitorName}!, the temperature is ${currentTemperature}°C in ${city}`;
 
         res.json({
-            ip: clientIp,
+            ip,
             city,
             greeting,
         });
 
-        console.log(clientIp);
+        console.log(ip);
         console.log(city);
         console.log(greeting);
     } catch (error) {
