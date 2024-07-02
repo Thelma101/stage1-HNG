c4bf1969f9a2ea617777994eaceb8ae59ee55b73
DCTP-BE <akpata.thelma@gmail.com>
2024-07-02 13:06:49 +0100

    final commit

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 const port = 3001;
 
 const ipstackAccessKey = '7d065c730b8326a1b41caa942cf628fa';
+const ipstackApiUrl = `http://api.ipstack.com/check?access_key=${ipstackAccessKey}`;
+const meteoWeatherAPI = 'https://api.open-meteo.com/v1/forecast?';
 
 app.get('/api/hello', async (req, res) => {
     const personName = req.query.personName || 'Tee';
 
     try {
-        // Fetch location data
-        const locationUrl = await fetch(`https://api.ipstack.com/check?access_key=${ipstackAccessKey}`);
-        const locationData = await locationUrl.json();
+        // Fetch location data using Axios
+        const locationResponse = await axios.get(ipstackApiUrl);
+        const { ip, city } = locationResponse.data;
 
-        const { ip, country_name, city, latitude: lat, longitude: lon } = locationData;
+        // if (!latitude || !longitude) {
+        //     throw new Error('Location data not available');
+        // }
 
-        const message = `Hello ${personName} from ${country_name}`;
-        
+        //         {
+        //   "client_ip": "127.0.0.1", // The IP address of the requester
+        //   "location": "New York" // The city of the requester
+        //   "greeting": "Hello, Mark!, the temperature is 11 degrees Celcius in New York"
+        // }
+
+        const weatherResponse = await axios.get(`${meteoWeatherAPI}&latitude=${latitude}&longitude=${longitude}&current=temperature_2m&hourly=temperature_2m&forecast_days=1`);
+        const currentTemperature = weatherResponse.data.current.temperature_2m;
+
+        const greeting = `Hello ${personName}!, the temperature is ${currentTemperature}°C in ${city}`;
+
         res.json({
-            message,
             ip,
             city,
-            lat,
-            lon
+            greeting,
         });
 
-        // Fetch temperature data
-        const temperatureUrl = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`);
-        const temperatureData = await temperatureUrl.json();
-
-        const { temperature, windspeed, winddirection, weathercode, time } = temperatureData.current_weather;
-
-        console.log(`${message} - It's ${temperature}°C, ${windspeed} m/s from ${winddirection} at ${time}`);
-        
-        console.log(message);
+        console.log(ip);
+        console.log(location);
+        console.log(greeting);
     } catch (error) {
         console.error(error);
         res.status(500).json({
@@ -42,5 +48,5 @@ app.get('/api/hello', async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Hurray, server is connected on ${port}`);
+    console.log(`Hurray, server is connected on http://localhost:${port}`);
 });
